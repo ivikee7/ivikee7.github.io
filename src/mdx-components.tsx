15ce130bad9a71407b200e97cfc671d2b61a946e
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2024-12-07 15:20:37 +0530

    nextjs

--- src/mdx-components.tsx
+++
@@ -1,8 +0,0 @@
-import type { MDXComponents } from 'mdx/types'
-import Image, { ImageProps } from 'next/image'
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-    return {
-        ...components,
-    }
-}
