8ca6566f0161b72cff1a4078a69598abcad87a38
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-02-26 14:43:50 +0530

    next

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,10 @@ const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
-}
+};
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
 })
 
-// Merge MDX config with Next.js config
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Hello, Next.js!</h1>
+      <h2>Hello, Next.js!</h2>
+      <h3>Hello, Next.js!</h3>
+      <h4>Hello, Next.js!</h4>
+      <h5>Hello, Next.js!</h5>
+      <h6>Hello, Next.js!</h6>
+    </div>
+  );
 }
