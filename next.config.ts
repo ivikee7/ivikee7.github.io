cddd59fd0f46401ee1485ffdec2120fd3a42e290
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-05-20 17:15:46 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  extension: /\.mdx?$/,
 };
 
-export default nextConfig;
+const nextConfig = {
+  // Important for GitHub Pages static export:
+  output: 'export',
+  trailingSlash: true, // Ensures compatibility with static hosting
+  images: { unoptimized: true }, // Required when using `next export`
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+};
+
+export default withMDX(nextConfig);
