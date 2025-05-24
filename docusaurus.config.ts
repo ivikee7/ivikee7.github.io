d008bdf756a2a2bc54dcbe51b79ba283e939969b
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-05-24 19:45:32 +0530

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,7 +51,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ivikee7/ivikee7.github.io',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -82,7 +82,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/ivikee7.github.io',
+          href: 'https://github.com/ivikee7/ivikee7.github.io',
           label: 'GitHub',
           position: 'right',
         },
