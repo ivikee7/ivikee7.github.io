3c52bd6a7d9e269a9e9de2dea17415f473665779
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-05-30 09:33:22 +0530

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ivikee7/ivikee7.github.io',
+            'https://github.com/ivikee7/ivikee7.github.io/tree/main/',
         },
         blog: {
           showReadingTime: true,
@@ -51,7 +51,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ivikee7/ivikee7.github.io',
+            'https://github.com/ivikee7/ivikee7.github.io/tree/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
