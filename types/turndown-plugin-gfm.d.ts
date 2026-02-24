0954526ae2f37fb619200dc995fd80e821a97e87
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2026-02-24 22:07:22 +0530

    Delete types directory

--- types/turndown-plugin-gfm.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "joplin-turndown-plugin-gfm" {
-  import { Plugin } from "turndown";
-
-  export const gfm: Plugin;
-  export const strikethrough: Plugin;
-  export const tables: Plugin;
-}
